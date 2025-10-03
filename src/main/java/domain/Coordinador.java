92b34ab89deca3c4ca459ed092dfb90d0029ff50
LeoZaid16 <leonardo-zaid@outlook.es>
2025-10-03 09:49:04 -0600

    Agrego comentario a clase Tutor. Se agrego la clase de Coordinador.

---
+++ src/main/java/domain/Coordinador.java
@@ -0,0 +1,44 @@
+/*
+*
+*   ITESS-TICS TInvestigación 1
+*   Agosto - Diciembre 2025
+*   Proyecto: Ni idea
+*   Clase Coordinador: Interesado en el rendimiento general del grupo TI701.
+*   Docente: FJMP
+*   Programador: Leonardo Zaid Moreno Jimenez
+*   Fecha: 02/Octubre/2025
+*
+*/
+
+package domain;
+
+/**
+ *
+ * @author TICS03
+ */
+public class Coordinador {
+    private String nombre;
+    private String area;
+
+    public Coordinador(String nombre, String area) {
+        this.nombre = nombre;
+        this.area = area;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+        
+}
